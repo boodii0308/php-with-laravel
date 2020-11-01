7f9c043a2b7fa1811641b7b07de3c0b38e554734
boodii0308 <boodii.tb@gmail.com>
2020-10-31 23:36:16 -0700

    first

---
+++ instagram/resources/views/home.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div>
+        <div class="row">
+            <div class="col-3 p-5">
+            <div><img src="https://scontent-sjc3-1.cdninstagram.com/v/t51.2885-19/s150x150/122268320_794584827784861_5888459713673383671_n.jpg?_nc_ht=scontent-sjc3-1.cdninstagram.com&_nc_ohc=KCBZhpl9egoAX8OUuhm&oh=8c17872c4ead699151fdcfc66b33c81c&oe=5FC70151" class = "rounded-circle"></div>
+            </div>
+            <div class="col-9 p-5" >
+              <div class = "pl-3 pt-1"> <h1>KaiGram</h1> </div>
+              <div class="d-flex">
+                  <div class = "pr-5"><strong>1k</strong> posts</div>
+                  <div class = "pr-5"><strong>100m</strong> followers</div>
+                  <div class = "pr-5"><strong>100</strong> following</div>
+              </div>
+              <div class = "pt-4" > Чинь сэтгэлээсээ инээмсэглэж чаддаг хүн л хайр татам.❤️ </div>
+              <div class = "pt-4" > <a href="#">www.facebook.com/Iv4nKa</a>  </div>
+            </div>
+        </div>
+        <div class="row pt-4">
+            <div class="col-4 ">
+                <img src="https://picsum.photos/300/300" class= "w-100 h-80 p-2">
+                </div>
+            <div class="col-4 ">
+                <img src="https://picsum.photos/400/400" class= "w-100 h-80 p-2">
+                </div>
+            <div class="col-4 ">
+                <img src="https://picsum.photos/300/300" class= "w-100 h-80 p-2">
+                </div>
+        </div>
+    </div>
+</div>
+@endsection
